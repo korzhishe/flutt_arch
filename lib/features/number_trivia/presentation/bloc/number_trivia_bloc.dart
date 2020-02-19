70ded8f459b1dd37233f70bf6135635ad90cbbb5
Kozhemyakin <agkozhemyakin@gmail.com>
2020-02-19 21:50:47 +0300

    lesson10

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,10 +1,19 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:flutter_architect/core/util/input_converter.dart';
+import 'package:flutter_architect/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:flutter_architect/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import './bloc.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(this.getConcreteNumberTrivia, this.getRandomNumberTrivia, this.inputConverter);
+
   @override
-  NumberTriviaState get initialState => InitialNumberTriviaState();
+  NumberTriviaState get initialState => Empty();
 
   @override
   Stream<NumberTriviaState> mapEventToState(
--- lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -1,8 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
+@immutable
 abstract class NumberTriviaEvent extends Equatable {
-  const NumberTriviaEvent();
+  NumberTriviaEvent();
 
   @override
-  List get props => [];
+  List<Object> get props => [];
 }
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString);
+
+  @override
+  List<Object> get props => [numberString];
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {}
